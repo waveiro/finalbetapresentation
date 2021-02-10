8cb28f6fddec2042d8d4d93d47d02f7a12a0c4d1
Willian Arsenio de Sousa <waveiro@hotmail.com>
2021-02-10 09:40:41 +0000

    00.1

--- src/00-ArrowFunctions.js
+++ src/00-ArrowFunctions.js
@@ -1,9 +1,15 @@
 const values = [1, 2, 3, 4];
 
-values.filter((value) => value % 2 === 0);
-
 values.filter(filterEvenValues);
 
 function filterEvenValues(value) {
   return value % 2 === 0;
 }
+
+values.filter((value) => {
+  return value % 2 === 0;
+});
+
+values.filter((value) => value % 2 === 0);
+
+values.filter(value => value % 2 === 0);
